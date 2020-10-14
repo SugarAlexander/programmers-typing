ab3f6878af27ff2986cf2538fd3df9f90ba8d94b
SugarAlexander <62225123+SugarAlexander@users.noreply.github.com>
2020-10-14 11:39:53 +0900

    #20

--- main.js
+++ main.js
@@ -7,28 +7,14 @@ canvas.width = 1920;
 canvas.height = 1080;
 
 function notu() {
-
-
-    //ctx.strokeRect(0,0,80,80);
-    // ctx.fillRect(100,0,80,80);
-   //ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-
-
-
     document.body.appendChild(canvas);
-
     draw();
 }
 
 
 
 function draw() {
-
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillRect(600, x++, 100, 10);
-
     window.requestAnimationFrame(draw);
-
-
 }
