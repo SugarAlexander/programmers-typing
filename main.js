21956b435754c7bd05be5f4984769bc628ef0127
SugarAlexander <62225123+SugarAlexander@users.noreply.github.com>
2020-10-20 17:23:48 +0900

    ノーツを降らせるプログラムを変更しました

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let x = 0;
+//let x = 0;
 
-const notes = []; // ノーツを管理する配列
+let notes = []; // ノーツを管理する配列
 
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext('2d');
@@ -15,9 +15,9 @@ canvas.width = 1920;
 canvas.height = 1080;
 
 notes.push({
-    lane: 0, // 左端のレーン
-    progress: 0 // 進み具合
-  });
+  lane: 1, // 左端のレーン
+  progress: 0 // 進み具合
+});
 
 function notu() {
     document.body.appendChild(canvas);
@@ -29,17 +29,20 @@ function notu() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     //ctx.fillRect(600, x++, 100, 10);
+    
     notes.forEach(note => {
         // 例えば fillRect で描く場合
         ctx.fillRect(
           LEFT_OFFSET + note.lane * NOTE_WIDTH, // X座標
           TOP_OFFSET + note.progress * NOTE_SCREEN_HEIGHT, // Y座標
           NOTE_WIDTH, NOTE_HEIGHT);
-          note.progress += 0.1;
+          note.progress += 0.005;
+
+          notes = notes.filter(note => note.progress < 1.0);
       });
 
     
       // 画面外のノーツを削除
-      notes = notes.filter(note => note.progress < 1.0);
+     
     window.requestAnimationFrame(draw);
 }
