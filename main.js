86a6dfc72a47ecbcb8bc533194143391277eb373
SugarAlexander <62225123+SugarAlexander@users.noreply.github.com>
2020-10-21 10:06:37 +0900

    #12

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 
 let notes = []; // ノーツを管理する配列
 
-const canvas = document.createElement("canvas");
-const ctx = canvas.getContext('2d');
+let canvas;
+let ctx;
 
 const LEFT_OFFSET = 100;
 const TOP_OFFSET = 30;
@@ -11,24 +11,16 @@ const NOTE_WIDTH = 150; // ノーツの横幅
 const NOTE_HEIGHT = 50; // ノーツの高さ
 const NOTE_SCREEN_HEIGHT = 800; // ノーツを表示する領域の高さ
 
-canvas.width = 1920;
-canvas.height = 1080;
-
-notes.push({
-  lane: 0, // 左端のレーン
-  progress: 0 // 進み具合
-});
-
-
-
-function notu() {
-    document.body.appendChild(canvas);
-    draw();
+function init() {
+    canvas = document.querySelector("canvas");
+    ctx = canvas.getContext("2d");
+    tick();
     window.setTimeout("hoge()", 1000);
     window.setTimeout("hoge1()", 2000);
     window.setTimeout("hoge2()", 3000);
     window.setTimeout("hoge3()", 4000);
     window.setTimeout("hoge4()", 5000);
+
 }
 
 function hoge(){
@@ -61,25 +53,38 @@ function hoge3(){
     progress: 0 // 進み具合
   });
 }
+window.addEventListener("DOMContentLoaded", init);
 
-
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //ctx.fillRect(600, x++, 100, 10);
-    
-    notes.forEach(note => {
-        // 例えば fillRect で描く場合
-        ctx.fillRect(
-          LEFT_OFFSET + note.lane * NOTE_WIDTH, // X座標
-          TOP_OFFSET + note.progress * NOTE_SCREEN_HEIGHT, // Y座標
-          NOTE_WIDTH, NOTE_HEIGHT);
-          note.progress += 0.005;
-
-          notes = notes.filter(note => note.progress < 1.0);
-      });
-
-    
-      // 画面外のノーツを削除
-     
-    window.requestAnimationFrame(draw);
+function tick() {
+    update();
+    render();
+    window.requestAnimationFrame(tick);
 }
+
+/**
+ * 更新
+ */
+function update() {
+   
+}
+
+/**
+ * 描画
+ */
+function render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    notes.forEach(note => {
+      // 例えば fillRect で描く場合
+      ctx.fillRect(
+        LEFT_OFFSET + note.lane * NOTE_WIDTH, // X座標
+        TOP_OFFSET + note.progress * NOTE_SCREEN_HEIGHT, // Y座標
+        NOTE_WIDTH, NOTE_HEIGHT);
+        note.progress += 0.005;
+
+        notes = notes.filter(note => note.progress < 1.0);
+    });
+
+  
+  
+}
+
